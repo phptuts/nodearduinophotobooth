3565296dc9d21b93c3cbeb131691fbfadcbc01a7
Noah Glaser <glaserpower@gmail.com>
2017-05-06 18:07:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var five = require("johnny-five");
+var express = require('express');
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var app = express();
+app.use('/socket-io', express.static(__dirname + '/node_modules/socket.io-client/dist'));
+
+
+five.Board().on("ready", function() {
+
+    var isConnected = false;
+
+    var button = new five.Button({
+        pin: 2,
+        isPullup: true
+    });
+
+    io.on('connection', function(){
+        console.log('connected');
+        isConnected = true;
+    });
+
+
+
+    button.on("up", function() {
+        if (isConnected) {
+            setTimeout(function () {
+                io.sockets.emit('take-picture', {'taking': true});
+            }, 3000);
+        }
+    });
+
+
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    });
+
+
+    app.listen(3000, function () {
+        console.log('localhost:3000 is on');
+    });
+
+    server.listen(1800);
+});
